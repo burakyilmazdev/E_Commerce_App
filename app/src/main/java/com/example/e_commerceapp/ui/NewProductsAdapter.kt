49de5acaba4336223fcde9874942a10051ca7f44
burakyilmazdev <burakyilmaz1627@gmail.com>
2022-01-25 20:15:15 +0300

    New Products Recycler View adapter has been written. New Products page has been created by using Data Binding library.

---
+++ app/src/main/java/com/example/e_commerceapp/ui/NewProductsAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.e_commerceapp.ui
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.e_commerceapp.data.models.Products
+import com.example.e_commerceapp.databinding.NewProductItemBinding
+import com.squareup.picasso.Picasso
+
+class NewProductsAdapter : RecyclerView.Adapter<NewProductsAdapter.ProductListViewHolder>(){
+
+    private val productList = arrayListOf<Products>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setProductList(productList : List<Products>){
+        this.productList.clear()
+        this.productList.addAll(productList)
+        notifyDataSetChanged()
+    }
+
+    class ProductListViewHolder(private val binding : NewProductItemBinding):
+        RecyclerView.ViewHolder(binding.root){
+
+            fun bind(productItem : Products){
+                Picasso.get().load(productItem.url).into(binding.ivProductImage)
+                binding.tvProductName.text = productItem.name
+                binding.tvProductPrice.text = productItem.price
+                binding.executePendingBindings()
+            }
+
+        companion object{
+
+            fun create(parent:ViewGroup):ProductListViewHolder{
+                val binding = NewProductItemBinding.inflate(LayoutInflater.from(parent.context))
+                return ProductListViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductListViewHolder {
+        return ProductListViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ProductListViewHolder, position: Int) {
+        holder.bind(productList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return productList.size
+    }
+}
--- app/src/main/java/com/example/e_commerceapp/ui/NewProductsFragment.kt
+++ app/src/main/java/com/example/e_commerceapp/ui/NewProductsFragment.kt
@@ -2,7 +2,6 @@ package com.example.e_commerceapp.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,19 +9,19 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.e_commerceapp.R
 import com.example.e_commerceapp.data.ProductDataSource
-import com.example.e_commerceapp.data.request.ServiceProvider
+import com.example.e_commerceapp.data.models.Status
 import com.example.e_commerceapp.databinding.NewProductsFragmentBinding
-import com.example.e_commerceapp.databinding.PopularProductsFragmentBinding
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 
 class ProductsFragment : Fragment() {
 
 
-    val productDataSource = ProductDataSource()
+    private val productDataSource = ProductDataSource()
     private lateinit var binding : NewProductsFragmentBinding
+    private val newProductsAdapter = NewProductsAdapter()
+
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +32,16 @@ class ProductsFragment : Fragment() {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
-                Log.v("TEST","${it.data.toString()}")
+                when(it.status){
+                    Status.SUCCESS ->{
+                        newProductsAdapter.setProductList(it.data!!)
+                        binding.newProductProgressBar.visibility = View.GONE
+                    }
+                    Status.LOADING->{
+                        binding.newProductProgressBar.visibility = View.VISIBLE
+
+                    }
+                }
             }
 
 
@@ -44,7 +52,10 @@ class ProductsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.new_products_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.new_products_fragment,container,false)
+
+        binding.newProductsRecyclerView.adapter = newProductsAdapter
+        return binding.root
     }
 
 
